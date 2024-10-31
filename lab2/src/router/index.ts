3f88075c9533d6f18c44b7811413e86aa8942c04
Tenderness <1186602926@qq.com>
2024-10-31 09:33:54 +0800

    Lab3 to 5.1

--- lab2/src/router/index.ts
+++ lab2/src/router/index.ts
@@ -6,7 +6,7 @@ import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
-
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -56,6 +56,18 @@ const router = createRouter({
       name:'student-information',
       component:StudentInformation
     },
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    }
+    
   ],
 })
 
