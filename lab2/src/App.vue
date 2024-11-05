b34fb89186c872fff1c216e06d1ca551433ee75d
Tenderness <1186602926@qq.com>
2024-11-05 17:32:29 +0800

    Lab5 to 3.3

--- lab2/src/App.vue
+++ lab2/src/App.vue
@@ -8,7 +8,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-<div id="layout">
+<div class="text-center font-sans text-gray-700 antialias">
  <header>
     <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -45,15 +45,6 @@ nav a.router-link-exact-active:hover {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
-}
-
-
-#layout{
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
